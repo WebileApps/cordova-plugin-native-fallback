acf69f716b785ded9f2a2209a85cbe49b0cc8c53
Rajiv Singaseni <rajiv@webileapps.com>
2025-07-25 20:30:11 +0530

    Fix: prevent WebView delegate conflicts on iOS and Android

--- src/android/NativeFallback.java
+++ src/android/NativeFallback.java
@@ -1,37 +1,58 @@
 package com.example.nativefallback;
 
-import org.apache.cordova.*;
-import android.webkit.WebView;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.engine.SystemWebViewClient;
+import org.apache.cordova.engine.SystemWebViewEngine;
+
+import android.app.AlertDialog;
+import android.os.Build;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
-import android.webkit.WebViewClient;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.os.Handler;
+import android.webkit.WebView;
 
 public class NativeFallback extends CordovaPlugin {
+
     @Override
-    public void initialize(final CordovaInterface cordova, final CordovaWebView webView) {
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
 
-        webView.getEngine().getView().setWebViewClient(new WebViewClient() {
-            private Handler handler = new Handler();
+        if (webView.getEngine() instanceof SystemWebViewEngine) {
+            SystemWebViewEngine engine = (SystemWebViewEngine) webView.getEngine();
+            
+            WebView androidWebView = (WebView) engine.getView();
 
-            @Override
-            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
-                showNativeErrorDialog();
-            }
+            androidWebView.setWebViewClient(new SystemWebViewClient(engine) {
+                
+                @Override
+                public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (request.isForMainFrame()) {
+                            showNativeErrorDialog();
+                        }
+                    }
+                }
 
-            private void showNativeErrorDialog() {
-                cordova.getActivity().runOnUiThread(() -> {
-                    new AlertDialog.Builder(cordova.getActivity())
-                        .setTitle("Network Error")
-                        .setMessage("Unable to load content. Please check your connection.")
-                        .setPositiveButton("Retry", (dialog, which) -> webView.loadUrl("https://your.remote.url"))
-                        .setCancelable(false)
-                        .show();
-                });
-            }
+                @Override
+                @SuppressWarnings("deprecation")
+                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                        showNativeErrorDialog();
+                    }
+                }
+            });
+        }
+    }
+
+    private void showNativeErrorDialog() {
+        cordova.getActivity().runOnUiThread(() -> {
+            new AlertDialog.Builder(cordova.getActivity())
+                .setTitle("Network Error")
+                .setMessage("Unable to load content. Please check your connection.")
+                .setPositiveButton("Retry", (dialog, which) -> webView.loadUrl("https://your.remote.url"))
+                .setCancelable(false)
+                .show();
         });
     }
 }
